e4078b7c54477efa9fd2c515056e6417d3040291
Tommy Mikkelsen <tm@orstrand.dk>
2014-09-28 00:10:12 +0200

    V0.0.0.3 See Changelog

--- Contents/Code/__init__.py
+++ Contents/Code/__init__.py
@@ -14,19 +14,28 @@
 #	Provided functions are:
 #
 # Returns the path to ~Library/Plex Media Server
-#		GetLibPath(Secret)						: Call like http://PMS:32400/utils/devtools?Func=GetLibPath&Secret=1234
+#		GetLibPath(Secret)
+#			Call like http://PMS:32400/utils/devtools?Func=GetLibPath&Secret=1234
+#
 # Returns the contents of an xml file
-# 	GetXMLFile(Secret, Path)			: Call like http://PMS:32400/utils/devtools?Func=GetXMLFile&Secret=1234&Path=<Path to file>
+# 	GetXMLFile(Secret, Path)
+#			Call like http://PMS:32400/utils/devtools?Func=GetXMLFile&Secret=1234&Path=<Path to file>
+#
 # Return the version of this plugin
-# 	GetVersion(Secret)						: Call like http://PMS:32400/utils/devtools?Func=GetVersion&Secret=1234
+# 	GetVersion(Secret)
+#			Call like http://PMS:32400/utils/devtools?Func=GetVersion&Secret=1234
+#
 # Return the contents of an OpenSubTitle XML file for a bundle
-#		GetOSXml(Secret, Bundle)	: Call like http://PMS:32400/utils/devtools?Func=GetOSXml&Secret=1234&Bundle=1e0f180c5eb1a91a2e8d10e341a3050ceb429449
+#		GetOSXml(Secret, Bundle)
+#			Call like http://PMS:32400/utils/devtools?Func=GetOSXml&Secret=1234&Bundle=1e0f180c5eb1a91a2e8d10e341a3050ceb429449
+#
+# Delete a subtitle file downloaded by the OpenSubtitle PlugIn
+#		#	DelOSSrt(Secret, Bundle, SrtFile)
+#			Call like: http://qnap2:32400/utils/devtools?Func=DelOSSrt&Secret=1234&Bundle=1e0f180c5eb1a91a2e8d10e341a3050ceb429449&SrtFile=94e38a0053dadb3c8ae0078c1571054f4fe65f96.srt
 #
 ####################################################################################################
 #TODO
-#	DelOSSrt(Secret, Bundle, SrtFile)
 # Hash Secret pwd.....Needs to be hashed as well on the client
-# Make a nice icon
 ####################################################################################################
 
 #**********  Imports needed *************
@@ -35,7 +44,7 @@ import urllib
 import os
 
 #********** Constants needed ************
-VERSION = '0.0.0.2'
+VERSION = '0.0.0.3'
 NAME = 'DevTools'
 PREFIX = '/utils/devtools'
 ART = 'art-default.jpg'
@@ -80,6 +89,8 @@ def MainMenu(Func='', Secret='', **kwargs):
 			return ERRORAUTH
 	elif Func=='GetOSXml':
 		return GetOSXml(Secret, kwargs.get("Bundle"))
+	elif Func=='DelOSSrt':
+		return DelOSSrt(Secret, kwargs.get("Bundle"), kwargs.get("SrtFile"))
 
 ####################################################################################################
 # Check Secret
@@ -131,6 +142,41 @@ def GetOSXml(Secret, Bundle):
 		document = et.parse( myFile )
 		root = document.getroot()
 		return et.tostring(root, encoding='utf8', method='xml')
+	else:
+		return ERRORAUTH
+
+####################################################################################################
+# Delete an OpenSubtitle downloaded srt file
+####################################################################################################
+''' Delete a subtitle file downloaded by OpenSubtitles
+		Returns ok if all goes well '''
+@route(PREFIX + '/DelOSSrt')
+def DelOSSrt(Secret, Bundle, SrtFile):
+	if PwdOK(Secret):
+		# Start by getting the OS XML file
+		myFile = os.path.join(Core.app_support_path, 'Media', 'localhost', Bundle[:1], Bundle[1:] + '.bundle', 'Contents', 'Subtitle Contributions', 'com.plexapp.agents.opensubtitles.xml')
+		Log.Debug('Getting contents of an OS XML file named %s' %(myFile))
+		document = et.parse( myFile )
+		root = document.getroot()
+		myResult = 'Not found'
+		myLanguages = root.findall('Language')		
+		for language in myLanguages:
+			if myResult=='Not Found':
+				myLang = language.get('code')
+				for myLangNode in language.findall('Subtitle'):
+					myMedia = myLangNode.get('media')
+					if myMedia == SrtFile:
+						myResult = os.path.join(myLang, myMedia)
+						break
+			else:
+				break	
+		# Now we got the filename and dir name, so let's nuke the file
+		nukeFile = os.path.join(Core.app_support_path, 'Media', 'localhost', Bundle[:1], Bundle[1:] + '.bundle', 'Contents', 'Subtitle Contributions', 'com.plexapp.agents.opensubtitles', myResult )
+		try:
+			os.remove(nukeFile)
+			return 'ok'
+		except OSError:
+			return 'error'
 	else:
 		return ERRORAUTH
 
